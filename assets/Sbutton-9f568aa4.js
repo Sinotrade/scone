ea92fd612fb6876a3090d2ae95ae65bb6ee23795
Yvictor <yvictor3141@gmail.com>
2023-03-11 17:31:14 +0800

    deploy

--- assets/Sbutton-9f568aa4.js
+++
@@ -1,1 +0,0 @@
-import{c as t,$ as a}from"./AppHeader-b7ad8ffc.js";var n=t({defaultClassName:"_3tqrh00",variantClassNames:{},defaultVariants:{},compoundVariants:[]});const o=a("button",n);export{o as S};
