4ebde96cef7631a264a23b24c3e53340e1932f6f
Yvictor <yvictor3141@gmail.com>
2023-03-16 10:48:36 +0800

    deploy

---
+++ assets/Sbutton-1b8cd126.js
@@ -0,0 +1,1 @@
+import{c as t,$ as a}from"./AppHeader-85e0a89f.js";var n=t({defaultClassName:"_3tqrh00",variantClassNames:{width:{n:"_3tqrh01",vw60:"_3tqrh02"}},defaultVariants:{width:"n"},compoundVariants:[]});const o=a("button",n);export{o as S};
--- assets/Sbutton-76e8d985.js
+++
@@ -1,1 +0,0 @@
-import{c as t,$ as a}from"./AppHeader-6967ca86.js";var n=t({defaultClassName:"_3tqrh00",variantClassNames:{width:{n:"_3tqrh01",vw60:"_3tqrh02"}},defaultVariants:{width:"n"},compoundVariants:[]});const o=a("button",n);export{o as S};
